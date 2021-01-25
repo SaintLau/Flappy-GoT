f82a9fe332a026504470a387cb9a790b0fcf585a
Laura Santos <alsr16p@gmail.com>
2021-01-25 15:58:21 +0000

    almost gameover

--- gameover.js
+++ gameover.js
@@ -1,6 +1,21 @@
 //const wallCanvas = document.getElementById('wall');
 //const context = wallCanvas.getContext('2d');
 
+//Define the conditional for the hit
+hitWith(obstacle) {
+    return !(this.bottom() < obstacle.top() ||
+             this.top() > obstacle.bottom() ||
+             this.right() < obstacle.left() ||
+             this.left() > obstacle.right())
+};
+
+//What happens if hit? 
 function gameOver() {
-    
+    const hit = obstacles.some((obstacle) => {
+        return character.hitWith(obstacle);
+    });
+
+    if(hit) {
+        game.stop();
+    }
 }
--- obstacles.js
+++ obstacles.js
@@ -32,6 +32,7 @@ function updateGame() {
         character.newPosition();
         character.update();
         updateObstacles();
+        gameOver();
 }
 
 
