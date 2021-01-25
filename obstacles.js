636ddc8fe34bf8b091ec5a573bb150cc531aa4a5
Laura Santos <alsr16p@gmail.com>
2021-01-25 10:48:08 +0000

    working on it

---
+++ obstacles.js
@@ -0,0 +1,110 @@
+const wallCanvas = document.getElementById('wall');
+const context = wallCanvas.getContext('2d');
+
+//this class will represent the obstacles in the game. Here it will be defined their behaviour
+let obstacles = []; //array to receive random values to generate obstacles
+
+//Here will be the main variable to make this work - Start, Stop, Points
+const game = {
+    frames: 0, //start has no moving
+    start: function() {
+        this.interval = setInterval(updateGame, 10);
+    },
+    //for game over
+    stop: function() {
+        clearInterval(this.interval);
+    },
+    clear: function() {
+        context.clearRect(0, 0, wallCanvas.width, wallCanvas.height);
+    },
+    //score: function() {
+    //
+    //}
+}
+
+//function to make the game run
+function updateGame() {
+        game.clear();
+        updateObstacles();
+}
+
+
+class Component {
+    constructor(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        this.speedX = 0;
+        this.speedY = 0;
+    }
+    update() { //to draw again and again
+        context.fillStyle = this.color;
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+    newPsition() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+    left() {
+        return this.x;
+    }
+    right() {
+        return this.x + this.width;
+    }
+
+    top() {
+        return this.y;
+    }
+    bottom() {
+        return this.y + this.height;
+    }
+}
+
+
+//function for obstacles to be updated and generated 
+function updateObstacles() {
+    for(let i = 0; i < obstacles.length; i++){
+        obstacles[i].x -= 1;
+        obstacles[i].update();
+    
+    
+    //the obstacles
+    game.frames += 1;
+    if (game.frames % 120 === 0) {
+
+    }
+            //push obstacles standart
+    let minHeight = 20;
+    let maxHeight = 340;
+    let height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+    let minGap = 50;
+    let maxGap = 340;
+    let gap = Math.floor(Math.random() + (maxGap - minGap + 1) + minGap);
+
+
+            //Top obstacle
+    let topObstacle = new Component(10,
+        height,
+        'orange',
+        wallCanvas.width,
+        0); //the value on y will be 0
+
+        obstacles.push(topObstacle);
+
+            //Bot obstacle
+    let botObstacle = new Component(10,
+        wallCanvas.width - height - gap,
+        'orange',
+        wallCanvas.width,
+        height + gap);
+
+        obstacles.push(botObstacle);
+    }
+
+    console.log(obstacles);
+}
+
+//to start
+game.start();
---
+++ wallBackground.js
@@ -0,0 +1,28 @@
+//const wallCanvas = document.getElementById('wall');
+//const context = wallCanvas.getContext('2d');
+
+const image = new Image();
+image.src = ' /Users/laurasantos/Desktop/ironhack/ProjectFlappyGoT/images/ice-game-of-thrones-a-song-of-ice-and-fire-the-wall-wallpaper-preview.jpg'
+const backgroundImage = {
+    image: image,
+    x: 0,
+    speed: 0,
+        move: function() {
+        this.x += this.speed; //-1 -2 -3 
+        this.x %= wallCanvas.width; //-500%500=0 => -1 -2 -3
+    },
+    draw: function() {
+        context.drawImage(this.image, this.x, 0);
+        context.drawImage(this.image, this.x + wallCanvas.width, 0);
+    }
+}
+
+function updateCanvas() {
+      backgroundImage.move();
+      context.clearRect(0, 0, wallCanvas.width, wallCanvas.height);
+      backgroundImage.draw();
+      
+      requestAnimationFrame(updateCanvas);
+}
+
+updateCanvas();
